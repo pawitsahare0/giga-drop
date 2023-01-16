b9dca93422019d4c53e358305f6a2ff3cb788f51
pawitsahare0 <119015232+pawitsahare0@users.noreply.github.com>
2023-01-16 16:50:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -137,7 +137,7 @@ sliceandsend(file);
    var fileSize = file.size;
    var name = file.name;
    var mime = file.type;
-   var chunkSize = 64 * 8192; // bytes
+   var chunkSize = 64 * 2048; // bytes
    var offset = 0;
    var sendProgress=0;
 
